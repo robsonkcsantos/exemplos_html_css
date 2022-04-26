e952e06d95e4a1781c8c5b244162e98033f44aa5
robson kerner <robson.santos@professor.unis.edu.br>
2022-04-26 00:14:43 -0300

    first commit

---
+++ meu_scrypt.js
@@ -0,0 +1,18 @@
+// alert('Olá criador!!!')
+document.getElementById("inputName").value = "alterando o valor"
+// variável do tipo string
+var texto = 'Variável do tipo string';
+
+// variável do tipo number
+var numInteiro = -10;
+var numFlutuante = 10.5; // double, float
+
+// variável boolean
+var x = true;
+
+// null , undefined
+var teste = null;
+var teste2;
+
+console.log(teste);
+console.log(teste2);
